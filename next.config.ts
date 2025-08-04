62e9cfe0aac9eb03495063003449fc190dc9d1a9
Agustin Granes <agustingranes@gmail.com>
2025-08-04 23:09:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'actc.org.ar',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
